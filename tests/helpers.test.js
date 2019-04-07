33fca8a72291a03ae238a06d46887c0445b902e1
ShubhamZanwar <zanwar.shubham@gmail.com>
2019-04-07 11:19:19 +0530

    Lint: Fixing lint errors

--- tests/helpers.test.js
+++ tests/helpers.test.js
@@ -1,13 +1,13 @@
 const helperFuncs = require('../src/helpers');
 
 describe('the greeting helper function', () => {
-    test('should return a string', () => {
-        expect(typeof helperFuncs.greet()).toEqual(typeof '');
-    });
+  test('should return a string', () => {
+    expect(typeof helperFuncs.greet()).toEqual(typeof '');
+  });
 
-    test('should return preppend the greeting to first function argument', () => {
-        const input = "Bob";
-        const expectedOutput = "Hello Bob";
-        expect(helperFuncs.greet(input)).toEqual(expectedOutput)
-    })
+  test('should return preppend the greeting to first function argument', () => {
+    const input = 'Bob';
+    const expectedOutput = 'Hello Bob';
+    expect(helperFuncs.greet(input)).toEqual(expectedOutput);
+  });
 });
